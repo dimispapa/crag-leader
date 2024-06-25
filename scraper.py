e42a0bcf13e1a61d6638a10123c68ef813e52c30
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-06-25 13:44:53 +0000

    feat: extract boulder info and construct objects
    pass boulders as a Crag class instance attribute for encapsulation

--- scraper.py
+++ scraper.py
@@ -9,6 +9,7 @@ class Crag:
     Attributes:
         routelist_url (str): The full URL containing the route list.
         session (requests.Session): The requests session for making HTTP requests.
+        boulders (list): List of Boulder instances associated with the crag.
     """
 
     def __init__(self, crag_url, headers):
@@ -21,23 +22,44 @@ class Crag:
         """
         self.crag_url = crag_url
         self.headers = headers
+
         # define full url containing routelist
         self.routelist_url = f"{self.crag_url}routelist"
+
         # initialize request session object
         self.session = requests.Session()
+
+        # call get_boulders method and pass boulders list as a crag attribute
+        self.boulders = self.get_boulders()
 
     def get_boulders(self):
         # get response from url
         response = self.session.get(
             url=self.routelist_url, headers=self.headers)
+
         # parse response html content
         soup = BeautifulSoup(response.content, 'html5lib')
+
         # locate anchor elements with "sector-item" class.
         # These contain the boulder pages, exclude the first one which is a combined list of all routes
         boulder_elements = soup.find_all(
             'a', attrs={'class': 'sector-item'})[1:]
 
-        return boulder_elements
+        boulders = []  # initialize empty boulders list
+
+        # loop through the boulder elements and extract the boulder name and url
+        for boulder_elem in boulder_elements:
+            # extract attributes from anchor element
+            name = boulder_elem.find(
+                'div', attrs={'class': 'name'}).text.strip()
+            url = boulder_elem['href']
+
+            # contstruct Boulder object and add to boulders list
+            boulder = Boulder(name, url)
+            boulders.append(boulder)
+
+        # return the boulders list
+        return boulders
 
 
 class Boulder:
@@ -69,5 +91,4 @@ CRAG_URL = "https://27crags.com/crags/inia-droushia/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
 inia_droushia_crag = Crag(CRAG_URL, HEADERS)
-boulder_elements = inia_droushia_crag.get_boulders()
-print(boulder_elements)
+print(inia_droushia_crag.boulders)
