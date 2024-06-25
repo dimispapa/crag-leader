05cd4e902f96d7b05133843db3948ff8e232e4b7
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-06-25 12:49:57 +0000

    feat: add the Boulder class
    to be used within the Crag class to initialize boulder instances

--- scraper.py
+++ scraper.py
@@ -22,9 +22,18 @@ class Crag:
 
     return boulder_elements
 
+class Boulder:
+  
+  def __init__(self, name, url):
+    self.name = name
+    self.url = url
+    
+  def __repr__(self):
+    return f"Boulder(name={self.name}, url={self.url})"
+
 # testing  
 CRAG_URL = "https://27crags.com/crags/inia-droushia/"
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
 inia_droushia_crag = Crag(CRAG_URL, HEADERS)
-test_soup, test_boulder_anchors = inia_droushia_crag.get_boulders()
-print(test_boulder_anchors)
+boulder_elements = inia_droushia_crag.get_boulders()
+print(boulder_elements)
