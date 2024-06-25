45c3575204897ff1580b1c665c9fcf8f0ee9fc5a
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-06-25 10:26:06 +0000

    refactor: create GoogleSheetsClient class to reuse
    refactor working code into class and functions to reuse later

--- scrape.py
+++ scrape.py
@@ -1,34 +1,46 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-# list APIs that the program has access to
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-]
+class GoogleSheetsClient:
+    def __init__(self, creds_file, scope):
+        self.creds_file = creds_file
+        self.scope = scope
+        self.client = self._authorize_client()
 
-# create scoped credentials from json file
-CREDS = Credentials.from_service_account_file("creds.json")
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+    def _authorize_client(self):
+        creds = Credentials.from_service_account_file(self.creds_file)
+        scoped_creds = creds.with_scopes(self.scope)
+        return gspread.authorize(scoped_creds)
 
-# create gspread authorized client
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+    def open_sheet(self, sheet_name):
+        return self.client.open(sheet_name)
 
-# access google sheets
-BOULDERS_GSHEET = GSPREAD_CLIENT.open("boulders")
-ROUTES_GSHEET = GSPREAD_CLIENT.open("routes")
-ASCENTS_GSHEET = GSPREAD_CLIENT.open("ascents")
+    def get_sheet_data(self, sheet_name, worksheet_name):
+        sheet = self.open_sheet(sheet_name)
+        worksheet = sheet.worksheet(worksheet_name)
+        return worksheet.get_all_values()
 
-# test that access works
-boulders_sheet = BOULDERS_GSHEET.worksheet("data")
-boulder_data = boulders_sheet.get_all_values()
-print(boulder_data)
+def main():
+    # Define the scope and credentials file
+    SCOPE = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    CREDS_FILE = "creds.json"
 
-routes_sheet = ROUTES_GSHEET.worksheet("data")
-route_data = routes_sheet.get_all_values()
-print(route_data)
+    # Create an instance of GoogleSheetsClient
+    gsc = GoogleSheetsClient(CREDS_FILE, SCOPE)
 
-ascents_sheets = ASCENTS_GSHEET.worksheet("data")
-ascent_data = ascents_sheets.get_all_values()
-print(ascent_data)
+    # Access and print data from boulders, routes, and ascents sheets
+    boulder_data = gsc.get_sheet_data("boulders", "data")
+    print("Boulder Data:", boulder_data)
+
+    route_data = gsc.get_sheet_data("routes", "data")
+    print("Route Data:", route_data)
+
+    ascent_data = gsc.get_sheet_data("ascents", "data")
+    print("Ascent Data:", ascent_data)
+
+if __name__ == "__main__":
+    main()
