b315a73beacf2bc208124f4f589346aad4e41a2f
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-06-25 12:40:44 +0000

    feat: add headers param to avoid potential errors

--- scraper.py
+++ scraper.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 
 class Crag:
 
-  def __init__(self, crag_url):
+  def __init__(self, crag_url, headers):
     self.crag_url = crag_url
+    self.headers = headers
     # define full url containing routelist
     self.routelist_url = f"{self.crag_url}routelist"
     # initialize request session object
@@ -12,19 +13,18 @@ class Crag:
 
   def get_boulders(self):
     # get response from url
-    response = self.session.get(self.routelist_url)
+    response = self.session.get(url=self.routelist_url, headers=self.headers)
     # parse response html content
     soup = BeautifulSoup(response.content, 'html5lib')
     # locate anchor elements with "sector-item" class.
     # These contain the boulder pages, exclude the first one which is a combined list of all routes
-    boulder_anchors = soup.find_all('a', attrs={'class':'sector-item'})[1:]
-    
-    boulders = [] # iniatilize empty boulders list
+    boulder_elements = soup.find_all('a', attrs={'class':'sector-item'})[1:]
 
-    return soup, boulder_anchors
+    return boulder_elements
 
 # testing  
 CRAG_URL = "https://27crags.com/crags/inia-droushia/"
-inia_droushia_crag = Crag(CRAG_URL)
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
+inia_droushia_crag = Crag(CRAG_URL, HEADERS)
 test_soup, test_boulder_anchors = inia_droushia_crag.get_boulders()
 print(test_boulder_anchors)
