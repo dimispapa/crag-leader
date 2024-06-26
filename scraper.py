0baeaefa51d38341727f8df051f92df8919e10ba
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-06-26 09:37:41 +0000

    refactor: use Scraper class in Crag class
    replace code for scraping in the crag class by passing the scraper obj

--- scraper.py
+++ scraper.py
@@ -42,13 +42,12 @@ class Crag:
 
     Attributes:
         crag_url (str): The base URL of the crag.
-        headers (dict): The HTTP headers to use for the requests.
+        scraper (Scraper): The scraper instance to handle HTTP requests and HTML parsing.
         routelist_url (str): The full URL containing the route list.
-        session (requests.Session): The requests session for making HTTP requests.
         boulders (list): List of Boulder instances associated with the crag.
     """
 
-    def __init__(self, crag_url, headers):
+    def __init__(self, crag_url, scraper):
         """
         Initialize Crag class instance.
 
@@ -57,13 +56,10 @@ class Crag:
             headers (dict): The HTTP headers to use for the requests.
         """
         self.crag_url = crag_url
-        self.headers = headers
+        self.scraper = scraper
 
         # define full url containing routelist
         self.routelist_url = f"{self.crag_url}routelist"
-
-        # initialize request session object
-        self.session = requests.Session()
 
         # call get_boulders method and pass boulders list as a crag attribute
         self.boulders = self.get_boulders()
@@ -75,12 +71,8 @@ class Crag:
         Returns:
             list: A list of Boulder instances.
         """
-        # get response from url
-        response = self.session.get(
-            url=self.routelist_url, headers=self.headers)
-
-        # parse response html content
-        soup = BeautifulSoup(response.content, 'html5lib')
+        # scrape parsed html content from url
+        soup = self.scraper.get(self.routelist_url)
 
         # locate anchor elements with "sector-item" class.
         # These contain the boulder pages, exclude the first one which is a combined list of all routes
@@ -159,5 +151,6 @@ class Route:
 CRAG_URL = "https://27crags.com/crags/inia-droushia/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
-inia_droushia_crag = Crag(CRAG_URL, HEADERS)
+scraper = Scraper(HEADERS)
+inia_droushia_crag = Crag(CRAG_URL, scraper)
 print(inia_droushia_crag.boulders)
