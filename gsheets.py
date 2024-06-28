2cd38fc3190dfb331ea434e5d040e988eab3870f
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-06-28 11:10:14 +0000

    feat: add write_data_to_sheet func
    also add try-except block to get_sheet_data func

--- gsheets.py
+++ gsheets.py
@@ -59,19 +59,49 @@ class GoogleSheetsClient:
         except gspread.SpreadsheetNotFound:
             gsheet = self.client.create(gsheet_name)
         return gsheet
-    
-    def get_sheet_data(self, sheet_name, worksheet_name):
+
+    def get_sheet_data(self, gsheet_name: str, worksheet_name: str):
         """
         Retrieve all values from a specific worksheet in a Google Sheet.
 
         Args:
-            sheet_name (str): The name of the Google Sheet.
+            gsheet_name (str): The name of the Google Sheet.
             worksheet_name (str): The name of the worksheet within the Google
                                     Sheet.
 
         Returns:
             list: A list of lists containing the worksheet data.
         """
-        sheet = self.open_sheet(sheet_name)
-        worksheet = sheet.worksheet(worksheet_name)
-        return worksheet.get_all_values()
+        try:
+            gsheet = self.client.open(gsheet_name)
+            worksheet = gsheet.worksheet(worksheet_name)
+            return worksheet.get_all_values()
+
+        except gspread.SpreadsheetNotFound:
+            return print(f'Error: The Google Sheet "{gsheet_name}" '
+                         'does not exist, check the name and try again.')
+
+    def write_data_to_sheet(self,
+                            gsheet_name: str, worksheet_name: str,
+                            data: list, headers: list):
+        """
+        Writes data to worksheet in Google Sheet.
+
+        Args:
+            gsheet_name (str): The name of the Google Sheet.
+            worksheet_name (str): The worksheet of the Google Sheet.
+            data (list): The list of dictionaries representing the data.
+            headers (list): The list of defined header name strings.
+        """
+
+        # first open or create the gsheet
+        gsheet = self.get_or_create_gsheet(gsheet_name)
+        # access the worksheet
+        worksheet = gsheet.worksheet(worksheet_name)
+        # clear the worksheet
+        worksheet.clear()
+        # append the headers
+        worksheet.append_row(headers)
+        # iterate through the data and append row by row
+        for row in data:
+            worksheet.append_row(row)
