f9eb6bb7956139c940da0b58510a8db78cfcafc6
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-07-01 15:03:59 +0000

    refactor: bring out clear func for reuse accross
    otherwise circular issue

---
+++ clear.py
@@ -0,0 +1,12 @@
+"""
+Module contains clear helper function to clear the CLI.
+"""
+
+import os
+
+
+def clear():
+    """
+    Clear function to clean-up the terminal so things don't get messy.
+    """
+    os.system("cls" if os.name == "nt" else "clear")
